33fffb69a938d86f5a4a1e1976d15887813121de
ahnjaewon <ajw1259@gmail.com>
2021-05-15 21:44:46 +0900

    start manchester united web site

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        ahnjaewon
-      </h1>
-      <h2 class="subtitle">
-        ahnjaewon's cloud wen server
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<main>
+  <div><h1>맨체스터 유나이티드를 위한 웹사이트</h1></div>
+  <div><h2>Manchester United Football Club</h2></div><br/>
+  <p>영국 잉글랜드 그레이터맨체스터 주의 트래포드를 연고로 하는 프로 축구 클럽. 뉴턴 히스 LYR F.C.(Newton Heath LYR F.C.)란 이름으로 창설되었다.</p>
+</main>
+  
+
 </template>
-
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
---
+++ pages/upload.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "start manchester united web site"
+dit push -u origin master
