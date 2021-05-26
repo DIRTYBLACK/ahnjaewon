e378e441731822fa341608cdf0ed84a253dacdae
ahnjaewon <ajw1259@gmail.com>
2021-05-26 11:03:23 +0900

    start manchester united web site

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
-<template>
-<main>
-  <div><h1>맨체스터 유나이티드를 위한 웹사이트</h1></div>
-  <div><h2>Manchester United Football Club</h2></div><br/>
-  <p>영국 잉글랜드 그레이터맨체스터 주의 트래포드를 연고로 하는 프로 축구 클럽. 뉴턴 히스 LYR F.C.(Newton Heath LYR F.C.)란 이름으로 창설되었다.</p>
-</main>
-  
 
+<template>
+  <div>
+    <section class="hero is-danger">
+      <div class="hero-body">
+        <p class="title">
+          맨체스터 유나이티드
+        </p>
+        <p class="subtitle">
+          Manchester United
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
